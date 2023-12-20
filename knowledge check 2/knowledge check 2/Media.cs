0fd6db8c993b8d0ecc7604649120e1dff61dc504
Courtney Truong <courtneybtruong@gmail.com>
2023-12-20 00:21:12 -0500

    knowledge check 2 completed

---
+++ knowledge check 2/knowledge check 2/Media.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace knowledge_check_2
+{
+    public class Media
+    {
+        public string Title { get; set; }
+        public string Genre { get; set; }
+
+    }
+
+    public class Book : Media
+    {
+        public string Author { get; set; }
+        public int Pages { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1} {2} {3}", Title, Genre, Author, Pages);
+        }
+
+    }
+}
---
+++ knowledge check 2/knowledge check 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace knowledge_check_2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("How many records do you want to add? ");
+            var numberOfRecords = int.Parse(Console.ReadLine());
+
+            var recordList = new List<Book>();
+            for (int i = 0; i < numberOfRecords; i++)
+            {
+                // In this loop, populate the object's properties using Console.ReadLine()
+                var book = new Book();
+                Console.WriteLine("Enter the value for Title");
+                book.Title = Console.ReadLine();
+                Console.WriteLine("Enter the value for Genre");
+                book.Genre = Console.ReadLine();
+                Console.WriteLine("Enter the value for Author");
+                book.Author = Console.ReadLine();
+                Console.WriteLine("Enter the value for Pages");
+                book.Pages = int.Parse(Console.ReadLine());
+                recordList.Add(book);
+                Console.WriteLine("Media Added");
+                Console.WriteLine(book.ToString());
+            }
+        }
+    }
+}
